8a4430a27d49aae838c1ad169fad9d77f3099928
Honore Solomon <honoresolomon@hotmail.com>
2022-09-27 20:37:29 -0700

    added functions for operators and grabed elements

---
+++ app.js
@@ -0,0 +1,29 @@
+const numbers = document.querySelectorAll('[data-number')
+const operators = document.querySelectorAll('[data-operator')
+const allClear= document.querySelector('[data-all-clear')
+const equal = document.querySelector('[data-equals')
+const negPos = document.querySelector('[data-neg-pos')
+const percent = document.querySelector('[data-percent')
+
+class Calculator {
+    constructor(previousOperandTextEl, currentOperandTextEl)
+}
+
+
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const divide = (a, b) => a / b;
+
+const multiply = (a, b) => a * b;
+
+// const operate = (operator, a , b) => 
+
+// math functions
+
+// const power = (num, power) => Math.pow(num, power);
+
+// const factorial = (num) => {
+//   return num > 1 ? num * factorial(num - 1) : 1;
+// };
